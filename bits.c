7d01a7172f4a9ca54386119d00be4d96ce31dc2a
Guillaume Claret <dev@clarus.me>
2014-04-29 10:40:38 +0200

    The type bits_t is now local to bits.c

--- bits.c
+++ bits.c
@@ -3,48 +3,53 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-bits_t new() {
-  return (bits_t) {
-    .bits_byte = 0,
-    .bits_count = 0
+typedef struct {
+  int byte;
+  int count;
+} t;
+
+t new() {
+  return (t) {
+    .byte = 0,
+    .count = 0
   };
 }
 
-void write(bits_t * bits, int bit) {
-  bits->bits_byte *= 2;
-  bits->bits_byte += bit;
-  bits->bits_count++;
-  if (bits->bits_count == 8) {
-    fputc(bits->bits_byte, stdout);
-    bits->bits_byte = bits->bits_count = 0;
+void write_bit(t * bits, int bit) {
+  bits->byte *= 2;
+  bits->byte += bit;
+  bits->count++;
+  if (bits->count == 8) {
+    fputc(bits->byte, stdout);
+    bits->byte = bits->count = 0;
   }
 }
 
-void write_byte(bits_t * bits, int byte) {
+void write_byte(t * bits, int byte) {
   for (int i = 0; i < 8; i++)
-    write(bits, (byte >> (7 - i)) & 1);
+    write_bit(bits, (byte >> (7 - i)) & 1);
 }
 
-void write_tree(bits_t * bits, const tree_t trees[], int index) {
+void write_tree(t * bits, const tree_t trees[], int index) {
   switch (trees[index].tree_kind) {
   case TREE_LEAF:
-    write(bits, 0);
+    write_bit(bits, 0);
     write_byte(bits, trees[index].tree_content.tree_leaf);
     break;
   case TREE_NODE:
-    write(bits, 1);
+    write_bit(bits, 1);
     write_tree(bits, trees, trees[index].tree_content.tree_node.tree_left);
     write_tree(bits, trees, trees[index].tree_content.tree_node.tree_right);
     break;
   }
 }
 
-void flush(bits_t * bits) {
-  fputc(bits->bits_byte << (8 - bits->bits_count), stdout);
+void flush(t * bits) {
+  fputc(bits->byte << (8 - bits->count), stdout);
 }
 
 void bits_write_file(const char file_name[], const tree_t trees[], int tree_index, int table[][NB_SYMBOLS]) {
-  bits_t bits = new(stdout);
+  t bits = new(stdout);
 
   // Print the total size of the file to compress in bytes.
   struct stat st;
@@ -63,7 +68,7 @@ void bits_write_file(const char file_name[], const tree_t trees[], int tree_inde
   int c;
   while ((c = fgetc(file)) != EOF)
     for (int i = 0; table[c][i] != -1; i++)
-      write(&bits, table[c][i]);
+      write_bit(&bits, table[c][i]);
   flush(&bits);
   fclose(file);
 }
--- bits.h
+++ bits.h
@@ -4,11 +4,6 @@
 #include "common.h"
 #include "tree.h"
 
-typedef struct {
-  int bits_byte;
-  int bits_count;
-} bits_t;
-
 void bits_write_file(const char file_name[], const tree_t trees[], int tree_index, int table[][NB_SYMBOLS]);
 
 #endif
